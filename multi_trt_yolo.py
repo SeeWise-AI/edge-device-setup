0a855b557896ebfb4f1d1931bcdf9298ec355ab4
abusheik008 <abusheikabdulrahim2021@gmail.com>
2024-06-13 12:20:21 +0530

    separated camera function to avoid conflicts

--- multi_trt_yolo.py
+++ multi_trt_yolo.py
@@ -15,7 +15,7 @@ import pycuda.autoinit  # This is needed for initializing CUDA driver
 
 # from utils.yolo_classes import get_cls_dict
 from utils.yolo_lnt_classes import get_cls_dict
-from utils.camera import add_camera_args, Camera
+from utils.camera_multi import add_camera_args, Camera
 from utils.display import open_window, set_display, show_fps
 from utils.visualization import BBoxVisualization
 from utils.yolo_with_plugins import TrtYOLO
